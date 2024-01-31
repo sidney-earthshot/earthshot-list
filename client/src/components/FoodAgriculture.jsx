934ee396855e4e1de49fc6762559720ef4aa2522
sidney-earthshot <sidney.pang@earthshot.ca>
2024-01-31 10:54:34 -0500

    completed supply subtab for foodAgriculture

--- client/src/components/FoodAgriculture.jsx
+++ client/src/components/FoodAgriculture.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Chart from "chart.js/auto";
 import ChartDataLabels from "chartjs-plugin-datalabels";
 Chart.register(ChartDataLabels);
-import { Pie } from "react-chartjs-2";
+import { Pie, Doughnut, Bar } from "react-chartjs-2";
 
 const subCategories = [
   "Supply",
@@ -18,7 +18,7 @@ const subCategories = [
 export default function FoodAgriculture() {
   const [currentSubTab, setCurrentSubTab] = useState("Supply");
 
-  const data = {
+  const pieData = {
     labels: ["Animal Based", "Plant Based"],
     datasets: [
       {
@@ -27,11 +27,12 @@ export default function FoodAgriculture() {
         backgroundColor: ["rgba(255, 99, 132, 0.2)", "rgba(54, 162, 235, 0.2)"],
         borderColor: ["rgba(255, 99, 132, 1)", "rgba(54, 162, 235, 1)"],
         borderWidth: 2,
+        offset: 10,
       },
     ],
   };
 
-  const options = {
+  const pieOptions = {
     plugins: {
       title: {
         display: true,
@@ -46,12 +47,118 @@ export default function FoodAgriculture() {
       datalabels: {
         color: "white",
         formatter: function (value) {
-          return `${value}g`;
+          return `${value} g`;
+        },
+        font: {
+          size: 20,
         },
       },
       tooltip: {
         callbacks: {
           label: (item) => `${item.dataset.label}: ${item.formattedValue}g`,
+        },
+      },
+    },
+  };
+
+  const doughnutData = {
+    labels: ["Minimum Daily Requirement", "Excess Calories"],
+    datasets: [
+      {
+        label: "Amount",
+        data: [1882, 1324],
+        backgroundColor: ["rgba(255, 99, 132, 0.2)", "rgba(54, 162, 235, 0.2)"],
+        borderColor: ["rgba(255, 99, 132, 1)", "rgba(54, 162, 235, 1)"],
+        borderWidth: 2,
+        offset: 10,
+      },
+    ],
+  };
+
+  const doughnutOptions = {
+    plugins: {
+      title: {
+        display: true,
+        text: "Caloric Supply per Capita (kcal)",
+        color: "white",
+      },
+      legend: {
+        labels: {
+          color: "white",
+        },
+      },
+      datalabels: {
+        color: "white",
+        formatter: function (value) {
+          return `${value} kcal`;
+        },
+        font: {
+          size: 15,
+        },
+      },
+      tooltip: {
+        callbacks: {
+          label: (item) => `${item.dataset.label}: ${item.formattedValue} kcal`,
+        },
+      },
+      subtitle: {
+        display: true,
+        color: "white",
+        text: "Daily Total: 3206 kcal",
+      },
+    },
+  };
+
+  const barData = {
+    labels: ["Total Protein", "Total Fat"],
+    datasets: [
+      {
+        label: "Amount",
+        data: [105.3, 105.15],
+        backgroundColor: ["rgba(255, 99, 132, 1)"],
+      },
+    ],
+  };
+
+  const barOptions = {
+    responsive: true,
+    plugins: {
+      legend: {
+        labels: {
+          color: "white",
+        },
+      },
+      title: {
+        display: true,
+        text: "Dietary Supply Breakdown",
+        color: "white",
+      },
+      datalabels: {
+        color: "white",
+        anchor: "end",
+        align: "top",
+        formatter: function (value) {
+          return `${value} g`;
+        },
+      },
+    },
+    scales: {
+      x: {
+        ticks: {
+          color: "white", // X-axis labels (ticks)
+        },
+        grid: {
+          color: "rgba(255, 255, 255, 0.1)", // X-axis lines
+          borderColor: "white", // X-axis border
+        },
+      },
+      y: {
+        ticks: {
+          color: "white", // Y-axis labels (ticks)
+        },
+        grid: {
+          color: "rgba(255, 255, 255, 0.1)", // Y-axis lines
+          borderColor: "white", // Y-axis border
         },
       },
     },
@@ -78,7 +185,13 @@ export default function FoodAgriculture() {
 
       {/* right */}
       <div className="w-5/6 bg-gray-600">
-        <Pie data={data} options={options} />
+        <div className="flex h-1/2 justify-center py-4">
+          <Pie data={pieData} options={pieOptions} />
+          <Doughnut data={doughnutData} options={doughnutOptions} />
+        </div>
+        <div className="flex h-1/2 justify-center py-4">
+          <Bar data={barData} options={barOptions} />
+        </div>
       </div>
     </div>
   );
